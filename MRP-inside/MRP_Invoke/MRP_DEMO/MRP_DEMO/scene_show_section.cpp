f2bd6a079518c8a2b219cbf3f5f1f01b134da448
jw8957 <jw8957@gmail.com>
2013-06-23 19:07:10 +0800

    Revert "MRP-inside"
    This reverts commit 2350cf6d85b4599a89cd6b08d461428b46188b23.

--- MRP-inside/MRP_Invoke/MRP_DEMO/MRP_DEMO/scene_show_section.cpp
+++
@@ -1,8 +0,0 @@
-#include "scene_show_section.h"
-
-int adjust_Scene_show_section(Scene_show_section* scene_show_section,unsigned char* buf)
-{
-	//buf 2 byte
-	scene_show_section->scene_id = buf[0] * 256 + buf[1];
-	return 0;
-}
